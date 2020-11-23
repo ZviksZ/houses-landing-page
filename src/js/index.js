839e5843c2d4d1f472f1ccc1065514fecd15abc3
Nikita Kapshtyk <kapshtyk.n@globalsolutions.ru>
2020-11-23 12:08:14 +0300

    подготовка проекта

---
+++ src/js/index.js
@@ -0,0 +1,37 @@
+import * as $ from 'jquery';
+import { CustomSelect } from './components/custom-select';
+import { CustomTabs } from './components/custom-tabs';
+import { Effects } from './components/effects';
+import { FeedForm } from './components/feed-form';
+import { initMaskedInput, initMoneyInput, initPlaceholders } from './components/form';
+import { Header } from './components/header';
+import { HeaderMenu } from './components/header-menu';
+import { ModalWindowFullScreen } from './components/modal-window-fullscreen';
+window.jQuery = require('jquery');
+
+$(function() {
+   // главное меню на мобильном
+   new Header();
+   new HeaderMenu();
+
+   new Effects();
+
+   // инициализация плагина кастомных селектов
+   new CustomSelect();
+
+   // функционал табов
+   new CustomTabs();
+
+   // форма подписки на рассылку новостей
+   new FeedForm();
+
+   // инициализация функционала модальных окон
+   let modal = new ModalWindowFullScreen();
+
+   // Инициализация плейсхолдеров и масок
+   initMaskedInput();
+   initPlaceholders();
+   initMoneyInput();
+
+
+});
